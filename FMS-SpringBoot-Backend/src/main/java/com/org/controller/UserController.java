7872cb690ea1df15f3ce3e42053a9f80206128ff
Kedar <kedar.kulkarni@ruckuswireless.com>
2022-10-23 20:44:49 +0530

    commit

--- FMS-SpringBoot-Backend/src/main/java/com/org/controller/UserController.java
+++ FMS-SpringBoot-Backend/src/main/java/com/org/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.org.model.Users;
 import com.org.service.UserService;
 
 @ComponentScan(basePackages = "com")
+@CrossOrigin(origins = "http://localhost:4200/*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
